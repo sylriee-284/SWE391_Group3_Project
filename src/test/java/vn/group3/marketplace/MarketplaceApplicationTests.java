8292ffce94d94cad938388eb4f7e3480336337d9
ThanhDinhLe <thanhldhe186993@fpt.edu.vn>
2025-10-21 03:32:27 +0700

    add

---
+++ src/test/java/vn/group3/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,16 @@
+// package vn.group3.marketplace;
+
+// import org.junit.jupiter.api.Test;
+// import
+// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest(properties = {
+// "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
+// })
+// class MarketplaceApplicationTests {
+
+// @Test
+// void contextLoads() {
+// }
+// }
